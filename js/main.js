48e5a543af2e23955abeccd9e337e124eec9f02b
yuelinwen <tyleryuelinwen@gmail.com>
2021-06-07 06:17:38 -0400

    all gd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+/*********************************************************************************
+*  WEB422 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ______Yuelin Wen________________ Student ID: ____114379209______________ Date: _____2021-06-06_______________
+*
+*
+********************************************************************************/
+
 var restaurantData = [];
 var currentRestaurant = {};
 var page = 1;
